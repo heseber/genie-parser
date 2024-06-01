56bd46a9f79201ced1fb6c09ded9de5056c69eaa
Henrik Seidel <henrik.seidel@bayer.com>
2024-06-01 14:26:25 +0200

    Fixed LICENSE

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="genie",
-    version="0.1.1",
+    version="0.1.2",
     url="https://github.com/Bayer-Group/genie-parser",
     author="Henrik Seidel",
     author_email="heseber+github@mailbox.org",
